3def70c2ece116f16b5b3d902101b965a702fb51
Blessed Sode <emi.square23@gmail.com>
2025-06-26 18:45:40 +0100

    submission

--- MySQLServer.py
+++ MySQLServer.py
@@ -6,8 +6,8 @@ cursor = mydb.cursor()
 try:
     cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     print("Database created successfully!")
-except Exception as e:
-    print("Error occurred while creating database:", e)
+except mysql.connector.Error as e:
+    print("Error occurred while creating database", e)
 
 cursor.close()
 mydb.close()
